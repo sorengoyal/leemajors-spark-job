4bcc2514e4ad6287498fbff43ed75ab841167aa3
sogoyal <sogoyal@expedia.com>
2016-07-21 11:58:18 -0500

    test case changed to make sure that Message processor works correctly

--- src/main/scala/com/expedia/www/leemajors/stat/aggregator/MessageProcessor.scala
+++ src/main/scala/com/expedia/www/leemajors/stat/aggregator/MessageProcessor.scala
@@ -4,7 +4,7 @@ import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.Logging
 import org.apache.spark.streaming.Duration
 import com.expedia.www.leemajors.spark.streaming.common.Result
-
+import org.apache.log4j.Logger
 case class MessageProcessor(metric: String, data: String, jobName: String) extends Result(jobName) {
 
   def this(data: String, jobName: String) = this("messageCount", data, jobName)
@@ -22,8 +22,13 @@ case class MessageProcessor(metric: String, data: String, jobName: String) exten
 object MessageProcessor extends Serializable with Logging {
 
   def apply(jobName: String, messages: DStream[String], windowDuration: Duration, slideDuration: Duration): DStream[MessageProcessor] = {
+    lazy val logger = Logger.getLogger("SampleJob.MessageProcessor")
+
     val messageStream = messages.
-      map(msg => (msg,1)).
+      map(msg => {
+        logger.info(msg)
+        (msg,1)
+      }).
       groupByKey().map(msg => new MessageProcessor(msg._1 + ","+ msg._2.toList.reduceLeft(_ + _), jobName))
     messageStream
   }
